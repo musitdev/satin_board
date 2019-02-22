7e6e4c86fe8a1df899020929e6503e4981c8a5bc
musitdev <philippe.delrieu@free.fr>
2019-02-22 14:37:12 +0100

    update connector board for PCB print, update Dac

--- software/applications/mpe_cv_converter/src/main.rs
+++ software/applications/mpe_cv_converter/src/main.rs
@@ -13,40 +13,42 @@
 
 // panic-handler crate
 extern crate panic_semihosting;
-extern crate stm32f7;
 extern crate satinapi;
+extern crate stm32f7;
 
 //mod dac;
 
-
-use rtfm::{app};
+use rtfm::app;
 use stm32f7xx_hal as hal;
 //use core::fmt::Write;
 //use cortex_m_rt::entry;
-use cortex_m_semihosting::{debug, hprintln}; //, hio
 use crate::hal::{
-    prelude::*,
-    device,
+    dac::{Dac, DacWord, DacWriter},
     delay::Delay,
-    timer::{Timer, Event as TimerEvent},
-    serial::{Rx, Serial, Tx, Error, Event},
-    dac::{Dac, DacWriter, DacWord},
+    device,
+    prelude::*,
+    serial::{Error, Event, Rx, Serial, Tx},
+    timer::{Event as TimerEvent, Timer},
 };
+use cortex_m_semihosting::{debug, hprintln}; //, hio
 use heapless;
 
 const CONST_TIMER_FREQ: u32 = 1;
 
-
 #[app(device = stm32f7::stm32f7x6)]
 const APP: () = {
-//    static mut ON: bool = false;
-//    static mut LED: hal::gpio::gpiob::PB7<hal::gpio::Output<hal::gpio::PushPull>> = ();
+    //    static mut ON: bool = false;
+    //    static mut LED: hal::gpio::gpiob::PB7<hal::gpio::Output<hal::gpio::PushPull>> = ();
     static mut TIMESTAMP: u64 = 0;
     static mut EVENT_TIMER: Timer<stm32f7::stm32f7x6::TIM2> = ();
-    static mut MESSAGE_BUF: heapless::spsc::Queue<satinapi::midi::MidiEvent, heapless::consts::U32> = ();
+    static mut MESSAGE_BUF: heapless::spsc::Queue<
+        satinapi::midi::MidiEvent,
+        heapless::consts::U32,
+    > = ();
     static mut TX: Tx<stm32f7::stm32f7x6::USART2> = ();
     static mut RX: Rx<stm32f7::stm32f7x6::USART2> = ();
-    static mut RX_BUF : satinapi::midi::MidiBuffer = ();
+    static mut RX_BUF: satinapi::midi::MidiBuffer = ();
+    static mut DAC: Dac = ();
 
     #[init] //(schedule = [blink])
     fn init() {
@@ -56,7 +58,8 @@ const APP: () = {
 
         let device: stm32f7::stm32f7x6::Peripherals = device;
         let gpiob = device.GPIOB.split();
-        let mut led: hal::gpio::gpiob::PB7<hal::gpio::Output<hal::gpio::PushPull>> = gpiob.pb7.into_push_pull_output();
+        let mut led: hal::gpio::gpiob::PB7<hal::gpio::Output<hal::gpio::PushPull>> =
+            gpiob.pb7.into_push_pull_output();
         led.set_high();
 
         let mut rcc = device.RCC.constrain();
@@ -69,14 +72,14 @@ const APP: () = {
         let tx = gpiod.pd5.into_alternate_af7();
         let rx = gpiod.pd6.into_alternate_af7();
         let mut serial = Serial::usart2(device.USART2, (tx, rx), 115_200.bps(), clocks);
- //   let serial = Serial::usart2(p.USART6, (tx, rx), 31_250.bps(), clocks);
-       serial.listen(Event::Rxne);
+        //   let serial = Serial::usart2(p.USART6, (tx, rx), 31_250.bps(), clocks);
+        serial.listen(Event::Rxne);
 
         let (tx, rx) = serial.split();
 
         //init dac
         hprintln!("before dac nit").unwrap();
-/*        let gpioa = device.GPIOA.split();
+        /*        let gpioa = device.GPIOA.split();
         gpioa.pa4.into_floating_input();
         gpioa.pa5.into_floating_input();
         rcc.apb1.enr().modify(|_, w| w.dacen().enabled()); //enable dac
@@ -97,19 +100,21 @@ const APP: () = {
 
         //init gpio
         let gpioa = device.GPIOA.split();
-        gpioa.pa4.into_floating_input();
-        gpioa.pa5.into_floating_input();
+        //Dac1 pin
+        let dac1_pin = gpioa.pa4.into_analog_output();
+        //Dac2 pin
+        let dac2_pin = gpioa.pa5.into_analog_output();
 
         let dac = Dac::new(device.DAC);
-        let (mut dac1, mut dac2) = dac.dacs();
+        let mut dac1 = dac.create_dac_channel1(dac1_pin);
         dac1.enable();
         dac1.disable_output_buffer();
         dac1.disable_trigger();
 
+        let mut dac2 = dac.create_dac_channel2(dac2_pin);
         dac2.enable();
         dac2.disable_output_buffer();
         dac2.disable_trigger();
-
 
         //test write
         dac1.write(DacWord::B8_ALIGN_R(128));
@@ -117,45 +122,81 @@ const APP: () = {
 
         hprintln!("end dac init").unwrap();
 
-        
+        //Gate PA9 + invertion gate: PI8
+        let gpioi = device.GPIOI.split();
+        let mut invertion_gate: hal::gpio::gpioi::PI8<hal::gpio::Output<hal::gpio::PushPull>> =
+            gpioi.pi8.into_push_pull_output();
+        invertion_gate.set_high();
+
+        //clock out PC6 INVERSION_CLOCK PI9
+        let mut invertion_clock: hal::gpio::gpioi::PI9<hal::gpio::Output<hal::gpio::PushPull>> =
+            gpioi.pi9.into_push_pull_output();
+        invertion_clock.set_high();
+
+        //CV Note: DAC_OUT1: PA4 SWITCH_NOTE: PA0
+
+        //CV vélocity: DAC_OUT2: PA5  SWITCH_VELOCITE: PA1
+
+        //Dac 1 et Dac 2 Dac 3 Dac 4 are controled via SPI2
+        //SPI2:
+        /*SPI2 GPIO Configuration
+        PI3     ------> SPI2_MOSI
+        PD3     ------> SPI2_SCK
+        PA8     ------> CS
+        */
+
+        //CV n3 DAC: DAC1_CLEAR:PI2  DAC1_DIN: PI3 DAC1_CS: PA8 DAC1_SCLK: PD3
+        //CV n3: SWITCH_CVn3_OUT:PB11  SWITCH_CV_n3: PB14
+
+        //DAC2: ZEROA_DAC2: PG9 DAC2_CS: PD8
+        //CV n4: SWITCH_CVn4_OUT: PB12 SWITCH_CV_n4: PH12
+
+        //DAC3_CS: PD0 FMT_DAC3: PI1 DEMP0_DAC3: PI0 DEMP1_DAC3: PH15 MUTE_DAC3:PH14 LRCK_DAC3:PI7 DATA_DAC3: PI6 BCK_DAC3: PI5 SCK_DAC3: PI4
+
+        //DAC4_CS: PD1
+        //CV out stéreo:   SWITCH_CV_STEREO OUT: PG2
+
+        //     SWITCH_CV_STEREO_EN:PH13
+
         EVENT_TIMER = systick;
         TX = tx;
         RX = rx;
         RX_BUF = satinapi::midi::MidiBuffer::new();
         MESSAGE_BUF = heapless::spsc::Queue::new();
+        DAC = dac;
     }
 
-/*    #[idle (resources = [LED])]
-    fn idle() -> ! {
-        
-//        resources.LED.set_high();
-        // error: `SHARED` can't be accessed from this context
-        // SHARED += 1;
-        hprintln!("idle").unwrap();
+    /*    #[idle (resources = [LED])]
+        fn idle() -> ! {
 
-        debug::exit(debug::EXIT_SUCCESS);
-        loop {}
-    } */
+    //        resources.LED.set_high();
+            // error: `SHARED` can't be accessed from this context
+            // SHARED += 1;
+            hprintln!("idle").unwrap();
+
+            debug::exit(debug::EXIT_SUCCESS);
+            loop {}
+        } */
 
     // `SHARED` can be access from this context
-     #[interrupt(resources = [EVENT_TIMER, TIMESTAMP])]
+    #[interrupt(resources = [EVENT_TIMER, TIMESTAMP])]
     fn TIM2() {
         *resources.TIMESTAMP = *resources.TIMESTAMP + 1;
         resources.EVENT_TIMER.start(CONST_TIMER_FREQ.khz());
-    } 
+    }
 
     #[interrupt(resources=[RX,RX_BUF, MESSAGE_BUF, TIMESTAMP], spawn = [manage_midi_input])]
     fn USART2() {
-//        hprintln!("USART2.").unwrap();
+        //        hprintln!("USART2.").unwrap();
         // Read each character from serial as it comes in
         match resources.RX.read() {
             Ok(c) => {
                 if let Some(message) = resources.RX_BUF.push_byte(c) {
-                    let event = satinapi::midi::MidiEvent{
+                    let event = satinapi::midi::MidiEvent {
                         message,
                         timestamp: *resources.TIMESTAMP,
                     };
-                    // TODO: handle buffer being full                
+                    // TODO: handle buffer being full
                     if resources.MESSAGE_BUF.enqueue(event).is_err() {
                         hprintln!("input serial queue full lost byte.").unwrap();
                     } else {
@@ -172,16 +213,16 @@ const APP: () = {
                     // it'll be stuck here
                     nb::Error::Other(hal::serial::Error::Overrun) => {
                         hprintln!("input serial queue overrun error.").unwrap();
-                    },
+                    }
                     nb::Error::Other(hal::serial::Error::Framing) => {
                         hprintln!("input serial queue Framing error.").unwrap();
-                    },
+                    }
                     nb::Error::Other(hal::serial::Error::Noise) => {
                         hprintln!("input serial queue Noise error.").unwrap();
-                    },
+                    }
                     nb::Error::Other(hal::serial::Error::Parity) => {
                         hprintln!("input serial queue Parity error.").unwrap();
-                    },
+                    }
                     _ => {
                         hprintln!("input serial queue other error.").unwrap();
                     }
@@ -189,15 +230,26 @@ const APP: () = {
             }
         }
     }
-    
-    #[task(resources=[MESSAGE_BUF, TX], capacity = 4)]
+
+    #[task(resources=[MESSAGE_BUF, TX, DAC], capacity = 4)]
     fn manage_midi_input() {
         //hprintln!("manage_usart_input").unwrap();
-        while let Some(event) = resources.MESSAGE_BUF.dequeue()    { 
-             //hprintln!("send event with time:{}", event.timestamp).unwrap();
-             let buf:[u8; 3] = event.message.into();  
-             let mut index = 0; 
-             while index < 3 {
+        while let Some(event) = resources.MESSAGE_BUF.dequeue() {
+            //hprintln!("send event with time:{}", event.timestamp).unwrap();
+            //for test set dac1 level depend on the note.
+            //note
+            resources
+                .DAC
+                .dac1()
+                .write(DacWord::B8_ALIGN_R(event.message.data1));
+            //Velocity
+            resources
+                .DAC
+                .dac2()
+                .write(DacWord::B8_ALIGN_R(event.message.data2));
+            let buf: [u8; 3] = event.message.into();
+            let mut index = 0;
+            while index < 3 {
                 match resources.TX.write(buf[index]) {
                     Ok(()) => index += 1,
                     Err(err) => {
@@ -205,17 +257,14 @@ const APP: () = {
                             nb::Error::WouldBlock => {
                                 //hprintln!("wouldblock.").unwrap();
                             }
-                            _ =>  { 
+                            _ => {
                                 hprintln!("Write serial error:{:?}", err).unwrap();
                                 break;
-                            },
-
+                            }
                         }
-                       
-                    },
-                }               
-             }   
-
+                    }
+                }
+            }
         }
     }
 
@@ -224,7 +273,6 @@ const APP: () = {
         fn USART1();
     }
 };
-
 
 /*#[entry]
 fn main() -> ! {
--- software/applications/satinapi/src/midi.rs
+++ software/applications/satinapi/src/midi.rs
@@ -1,3 +1,22 @@
+// These are midi status message types as sent on the wire
+const STATUS_EVENT_NOTE_OFF: u8 = 0x80;
+const STATUS_EVENT_NOTE_ON: u8 = 0x90;
+const STATUS_EVENT_VELOCITY_CHANGE: u8 = 0xA0;
+const STATUS_EVENT_CONTROL_CHANGE: u8 = 0xB0;
+const STATUS_EVENT_PROGRAM_CHANGE: u8 = 0xC0;
+const STATUS_AFTER_TOUCH: u8 = 0xD0;
+const STATUS_PITCH_CHANGE: u8 = 0xE0;
+const STATUS_START_PROPRIETARY: u8 = 0xF0;
+const STATUS_SONG_POSITION: u8 = 0xF2;
+const STATUS_SONG_SELECT: u8 = 0xF3;
+const STATUS_TUNE_REQUEST: u8 = 0xF6;
+const STATUS_END_PROPRIETARY: u8 = 0xF7;
+const STATUS_SYNC: u8 = 0xF8;
+const STATUS_START: u8 = 0xFA;
+const STATUS_CONTINUE: u8 = 0xFB;
+const STATUS_STOP: u8 = 0xFC;
+const STATUS_ACTIVE_SENSE: u8 = 0xFE;
+const STATUS_RESET: u8 = 0xFF;
 
 /// Represents a Midi message.
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
@@ -6,6 +25,17 @@ pub struct MidiMessage {
     pub data1: u8,
     pub data2: u8,
 }
+
+impl MidiMessage {
+    pub fn is_not_on(&self) -> bool {
+      self.status == STATUS_EVENT_NOTE_ON
+    }
+
+    pub fn is_not_off(&self) -> bool {
+      self.status == STATUS_EVENT_NOTE_OFF
+    }
+}
+
 impl From<[u8; 3]> for MidiMessage {
     fn from(raw: [u8; 3]) -> Self {
         MidiMessage {
@@ -16,14 +46,14 @@ impl From<[u8; 3]> for MidiMessage {
     }
 }
 impl Into<[u8; 3]> for MidiMessage {
-  fn into(self) -> [u8; 3] {
-    [self.status, self.data1, self.data2]
-  }
+    fn into(self) -> [u8; 3] {
+        [self.status, self.data1, self.data2]
+    }
 }
 impl Into<[u8; 3]> for &MidiMessage {
-  fn into(self) -> [u8; 3] {
-    [self.status, self.data1, self.data2]
-  }
+    fn into(self) -> [u8; 3] {
+        [self.status, self.data1, self.data2]
+    }
 }
 
 /// Represents a time stamped midi event. See also `MidiMessage`
@@ -35,34 +65,34 @@ pub struct MidiEvent {
 }
 
 pub struct MidiBuffer {
-	data: [u8;3],
-	index: usize,
+    data: [u8; 3],
+    index: usize,
 }
 
 impl MidiBuffer {
-	pub fn new() -> MidiBuffer {Default::default()}
+    pub fn new() -> MidiBuffer {
+        Default::default()
+    }
 
-	//add the specified byte to the buffer. Return a MidiEvent when it can be constructed.
-	pub fn push_byte(&mut self, byte: u8) -> Option<MidiMessage> {
-		self.data[self.index] = byte;
-		self.index +=1;
-		if self.index == 3 {
-			self.index =0;
-			let ret = [self.data[0], self.data[1], self.data[2]];
-			Some(MidiMessage::from(ret))
-		} else {
-			None
-		}
-	}
-
-
+    //add the specified byte to the buffer. Return a MidiEvent when it can be constructed.
+    pub fn push_byte(&mut self, byte: u8) -> Option<MidiMessage> {
+        self.data[self.index] = byte;
+        self.index += 1;
+        if self.index == 3 {
+            self.index = 0;
+            let ret = [self.data[0], self.data[1], self.data[2]];
+            Some(MidiMessage::from(ret))
+        } else {
+            None
+        }
+    }
 }
 
 impl Default for MidiBuffer {
-    fn default() -> MidiBuffer { 
-    	MidiBuffer {
-			data: [0,0,0],
-			index: 0,
-		}
+    fn default() -> MidiBuffer {
+        MidiBuffer {
+            data: [0, 0, 0],
+            index: 0,
+        }
     }
 }
